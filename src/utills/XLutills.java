91cd24cfdc175e393b91cacb6fadddd1ce82004e
RamaKrishna <RamaKrishna@DESKTOP-9O6SJ1M>
2022-06-27 00:01:34 +0530

    commit5; XLUttils uploaded

--- src/utills/XLutills.java
+++ src/utills/XLutills.java
@@ -5,43 +5,165 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openxmlformats.schemas.spreadsheetml.x2006.main.impl.WorksheetDocumentImpl;
+
+
 
 public class XLutills {
-
-	public static void main(String[] args) throws IOException   {
-		FileInputStream fi = new FileInputStream("C:\\Users\\RamaKrishna\\OneDrive\\Desktop\\testxlfile.xlsx");
-		Workbook wb = new XSSFWorkbook(fi);
-		wb.createSheet("ramusheet2");
-	    Sheet ws1=	wb.getSheet("Sheet1");
-        
-        Row row1=	ws1.getRow(1);
-        
-       Cell cell1=   row1.getCell(1);
-       
-       Cell cell2=row1.getCell(2);
-       
-      String str1= cell1.getStringCellValue();
-     String   str2=cell2.getStringCellValue();
-        
-      System.out.println(str1 + "    "+str2);
+    public static	FileInputStream fi;
+    public static FileOutputStream fo;
+	public static Workbook wb;
+	public static Sheet ws;
+	public static Row row;
+	private static int rownum;
+	public static Cell cell;
+	public static CellStyle style;
+	
+	
+	
+	public static int getRowCount (String xlfile, String xlsheet) throws IOException
+	{
+		fi=new FileInputStream(xlfile);
+		wb=new HSSFWorkbook(fi);
+	    ws=wb.getSheet(xlsheet);
+	    int  rowcount=ws.getLastRowNum();
+	    wb.close();
+	    fi.close();
+	    return  rowcount;
+	}
+      
+	public static short getColumnCount(String xlfile,String xlsheet,int rownum) throws IOException
+	{
+		fi= new FileInputStream(xlfile);
+		wb= new HSSFWorkbook(fi);
+		ws= wb.getSheet(xlsheet);
+	   row= ws.getRow(rownum);
+	   short   columncount= row.getLastCellNum();
+	   wb.close();
+		fi.close();
+		return columncount;
+	}
+	
+	public static String getStringCellData ( String xlfile,String  xlsheet,int rownum,int colnum) throws IOException
+	{
+	fi= new FileInputStream(xlfile);
+	wb=new HSSFWorkbook(fi);
+	ws= wb.getSheet(xlsheet);
+	row= ws.getRow(rownum);
+	String data;
+			try {
+				cell=row.getCell(colnum);
+				data=cell.getStringCellValue();
+				
+			} catch (Exception e) {
+				data=" ";
+			}
+	wb.close();
+	fi.close();
+	return data;
 		
-		FileOutputStream fo= new FileOutputStream("C:\\Users\\RamaKrishna\\OneDrive\\Desktop\\result.xlsx");
-		wb.write(fo);
-	  Sheet demosheet=	wb.createSheet("demosheet2");
-	  Row row0= demosheet.createRow(0);
-	  ;
-	Cell cell0=  row0.createCell(0 );
-	  System.out.println(cell0);
-	
-
-
+	}
+	public static double getNumaricCellData ( String xlfile,String  xlsheet,int rownum,int colnum) throws IOException
+	{
+	fi= new FileInputStream(xlfile);
+	wb=new HSSFWorkbook(fi);
+	ws= wb.getSheet(xlsheet);
+	row= ws.getRow(rownum);
+	double data;
+			try {
+				cell=row.getCell(colnum);
+				data=cell.getNumericCellValue();
+				
+			} catch (Exception e) {
+				data=0;
+			}
+	wb.close();
+	fi.close();
+	return data;
 }
+	public static boolean getBooleanCellData ( String xlfile,String  xlsheet,int rownum,int colnum) throws IOException
+	{
+	fi= new FileInputStream(xlfile);
+	wb=new HSSFWorkbook(fi);
+	ws= wb.getSheet(xlsheet);
+	row= ws.getRow(rownum);
+	boolean data;
+			try {
+				cell=row.getCell(colnum);
+				data=cell.getBooleanCellValue();
+				
+			} catch (Exception e) {
+				data=false;
+			}
+	wb.close();
+	fi.close();
+	return data;
+}
+	public static void setCellData(String xlfile,String  xlsheet,int rownum,int colnum,String data) throws IOException
+	{
+		fi= new FileInputStream(xlfile);
+		wb=new HSSFWorkbook(fi);
+		ws=wb.getSheet(xlsheet);
+		row=ws.getRow(rownum);
+		cell=row.createCell(colnum);
+		cell.setCellValue(data);
+		fo=new FileOutputStream(xlfile);
+		wb.write(fo);
+		wb.close();
+		fi.close();
+		fo.close();
+				
+	}
 	
+	public static void fillGreenColour (String xlfile,String  xlsheet,int rownum,int colnum) throws IOException
+	{
+		fi= new FileInputStream(xlfile);
+		wb=new HSSFWorkbook(fi);
+		ws=wb.getSheet(xlsheet);
+		row=ws.getRow(rownum);
+		cell=row.getCell(colnum);
+		
+		style= wb.createCellStyle();
+		style.setFillForegroundColor(IndexedColors.BRIGHT_GREEN.getIndex());
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		
+		
+		cell.setCellStyle(style);
+		fo=new FileOutputStream(xlfile);
+		wb.write(fo);
+		wb.close();
+		fi.close();
+		fo.close();
+				
+	}
+	
+	public static void fillRedColour (String xlfile,String  xlsheet,int rownum,int colnum) throws IOException
+	{
+		fi= new FileInputStream(xlfile);
+		wb=new HSSFWorkbook(fi);
+		ws=wb.getSheet(xlsheet);
+		row=ws.getRow(rownum);
+		cell=row.getCell(colnum);
+		
+		style= wb.createCellStyle();
+		style.setFillForegroundColor(IndexedColors.RED.getIndex());
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		
+		
+		cell.setCellStyle(style);
+		fo=new FileOutputStream(xlfile);
+		wb.write(fo);
+		wb.close();
+		fi.close();
+		fo.close();
+	
+}
 }
